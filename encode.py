96f0f943366e8c5d5cae6077e21b91dd05033fbf
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2022-03-15 23:25:16 +0530

    new images for final web v2.1.0 2

--- encode.py
+++ encode.py
@@ -7,23 +7,24 @@ pictures_in_dir = [
     pic for pic in os.listdir() if pic.endswith("jpg") or pic.endswith("png")
 ]
 width, height = 1920, 1080
+command = ""
 
 for pic in pictures_in_dir:
     img = Image.open(pic)
     w, h = img.size
     if w >= 1920:
-        pass
+        command = f"cwebp.exe -preset photo -q 50 -m 6 -mt -crop 80 80 {width} {height} {pic} -o {pic.split('.')[0] + '.webp'}"
     elif w >= 1280:
         width, height = 1280, 720
+        command = f"cwebp.exe -preset photo -q 50 -m 6 -mt -crop 0 0 {width} {height} {pic} -o {pic.split('.')[0] + '.webp'}"
     else:
-        width, height = w, h
+        command = f"cwebp.exe -preset photo -q 60 -mt {pic} -o {pic.split('.')[0] + '.webp'}"
+    
     # since cwebp.exe is staticly compiled, each it is thread-safe, so each graphic is given to each thread
     # of cwebp.exe for faster conversion
-    subprocess.Popen(
-        f"cwebp.exe -preset photo -q 50 -m 6 -mt -crop 80 80 {width} {height} {pic} -o {pic.split('.')[0] + '.webp'}"
-    )
+    subprocess.Popen(command)
     
     # after spawning new processes scripts needs to be stoped to keep handling spawned proccesses.
     # if not stopped, then after termination of loop handlers are garbage collected and script
     # hangs as handlers are no more in memory pool
-    sleep(5)
+    sleep(3)
